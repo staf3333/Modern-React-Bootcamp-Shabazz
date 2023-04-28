f88050a2a1c71782a44386ae2f50058a59d03753
staf3333 <staf3333@gmail.com>
2023-04-27 21:33:32 -0700

    4/27 Finishing Intro to Create-React

--- 04_Introducing_Create_React_App/cra_conventions/src/Dog.js
+++ 04_Introducing_Create_React_App/cra_conventions/src/Dog.js
@@ -1,10 +1,13 @@
 import { React, Component } from 'react'
+import tibMastiff from './TibMastiff.jpg'
+import './Dog.css'
 
 class Dog extends Component {
     render() {
         return (
-            <div>
+            <div className='Dog'>
                 <h1>Dog!</h1>
+                <img className='Dog-img' src={tibMastiff} />
             </div>
         );
     }
