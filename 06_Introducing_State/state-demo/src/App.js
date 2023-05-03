4282733a3ba9a59bda92b78ad61dd264f598db41
staf3333 <staf3333@gmail.com>
2023-05-02 20:28:26 -0700

    5/2

--- 06_Introducing_State/state-demo/src/App.js
+++ 06_Introducing_State/state-demo/src/App.js
@@ -4,14 +4,15 @@ import Game from './Game';
 import Game2 from './Game2';
 import Demo from './Demo';
 import Rando from './Rando';
+import Button from './Button';
+import BrokenClick from './BrokenClick';
+import BrokenClick2 from './BrokenClick2';
 
 function App() {
   return (
     <div className="App">
-      <Game />
-      <Game2 />
-      <Demo animal='Bobcat' food='Pineapple' />
-      <Rando maxNum={7} />
+      <BrokenClick />
+      <BrokenClick2 />
     </div>
   );
 }
