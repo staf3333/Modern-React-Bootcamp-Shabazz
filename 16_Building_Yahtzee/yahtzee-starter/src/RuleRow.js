1239fd19744b1715e00e53c17054acbe13fda306
Mustafa <staf3333@gmail.com>
2023-05-28 18:23:46 -0400

    finishing(ish) Yahtzee

--- 16_Building_Yahtzee/yahtzee-starter/src/RuleRow.js
+++ 16_Building_Yahtzee/yahtzee-starter/src/RuleRow.js
@@ -3,10 +3,11 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const { score, doScore, name } = this.props
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={`RuleRow RuleRow-${score ? "disabled" : "active"}`} onClick={score ? null : doScore}>
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
     )
   }
