0798a1bb634b4a82a91f62e0fbf86ef04848ed99
Mustafa <staf3333@gmail.com>
2023-06-05 19:11:51 -0400

    Add copy to clipboard!

--- Massive_Color_Project/react-colors-app/src/ColorBox.js
+++ Massive_Color_Project/react-colors-app/src/ColorBox.js
@@ -1,18 +1,22 @@
 import './ColorBox.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const ColorBox = (props) => {
     const { name, background } = props
     return (
-        <div style={{ background: background }} className="ColorBox">
-            <div className='copy-container'>
-                <div className='box-content'>
-                    <span>{name}</span>
+        <CopyToClipboard text={background}>
+            <div style={{ background: background }} className="ColorBox">
+                <div className='copy-container'>
+                    <div className='box-content'>
+                        <span>{name}</span>
+                    </div>
+                    <button className='copy-button'>Copy</button>
                 </div>
-                <button className='copy-button'>Copy</button>
+                <span className='see-more'>More</span>
             </div>
-            <span className='see-more'>More</span>
-        </div>
+        </CopyToClipboard>
     )
 }
 
 export default ColorBox;
+
