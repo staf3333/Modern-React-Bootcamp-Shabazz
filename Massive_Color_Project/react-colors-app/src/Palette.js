b62cd9835be70b53fbeae0a1245f653bc4e13ff7
Mustafa <staf3333@gmail.com>
2023-06-06 09:04:09 -0400

    adding slider

--- Massive_Color_Project/react-colors-app/src/Palette.js
+++ Massive_Color_Project/react-colors-app/src/Palette.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import { useState } from 'react';
 
 const Palette = (props) => {
-    const colorBoxes = props.colors.map(color => (
-        <ColorBox background={color.color} name={color.name} />
+    const [level, setLevel] = useState(500);
+    const { colors } = props.palette;
+    const colorBoxes = colors[level].map(color => (
+        <ColorBox background={color.hex} name={color.name} />
     ));
+    const changeLevel = (newLevel) => {
+        setLevel(newLevel);
+    }
     return (
         <div className='Palette'>
+            <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
             {/*Navbar goes here*/}
             <div className='Palette-colors'>
                 {/* bunch of color boxes */}
