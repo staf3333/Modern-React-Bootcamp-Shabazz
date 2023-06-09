a989a4e8a0685fb9cac4020c39a3ceb18b4b5edd
Mustafa <staf3333@gmail.com>
2023-06-09 16:14:28 -0700

    fix palette routes

--- Massive_Color_Project/react-colors-app/src/App.js
+++ Massive_Color_Project/react-colors-app/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Palette from './Palette';
-import seedColors from './seedColors';
 import { Routes, Route } from 'react-router-dom';
-import { generatePalette } from './colorHelpers';
+import PaletteWrapper from './PaletteWrapper';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<h1>Palette LIST GOES HERE!</h1>} />
-      <Route path='/palette/:id' element={<h1>INDIVIDUAL PALETTE</h1>} />
+      <Route path='/palette/:id' element={<PaletteWrapper />} />
     </Routes>
     // <div>
     //   <Palette palette={generatePalette(seedColors[4])} />
---
+++ Massive_Color_Project/react-colors-app/src/PaletteWrapper.js
@@ -0,0 +1,19 @@
+import { generatePalette } from './colorHelpers';
+import { useParams } from 'react-router-dom';
+import seedColors from './seedColors';
+import Palette from './Palette';
+
+const findPalette = id => {
+    return seedColors.find(function (palette) {
+        return palette.id === id;
+    });
+}
+
+const PaletteWrapper = () => {
+    const { id } = useParams();
+    const palette = generatePalette(findPalette(id));
+
+    return <Palette palette={palette} />
+}
+
+export default PaletteWrapper;
