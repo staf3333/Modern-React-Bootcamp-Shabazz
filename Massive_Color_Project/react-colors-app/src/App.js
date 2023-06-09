57d95d7b8439c2768d7ea27609289748f8bfd067
Mustafa <staf3333@gmail.com>
2023-06-09 15:40:49 -0700

    add react router

--- Massive_Color_Project/react-colors-app/src/App.js
+++ Massive_Color_Project/react-colors-app/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { Routes, Route } from 'react-router-dom';
 import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Routes>
+      <Route path='/' element={<h1>Palette LIST GOES HERE!</h1>} />
+      <Route path='/palette/:id' element={<h1>INDIVIDUAL PALETTE</h1>} />
+    </Routes>
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
