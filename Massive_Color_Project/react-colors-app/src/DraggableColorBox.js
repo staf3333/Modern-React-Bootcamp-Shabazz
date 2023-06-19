24670208b6341463be79e38294f1165be868c1dc
Mustafa <staf3333@gmail.com>
2023-06-18 20:29:28 -0700

    Style DraggableColorBox

--- Massive_Color_Project/react-colors-app/src/DraggableColorBox.js
+++ Massive_Color_Project/react-colors-app/src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const styles = {
     root: css({
@@ -9,14 +10,37 @@ const styles = {
         display: "inline-block",
         position: "relative",
         cursor: "pointer",
-        marginBottom: "-3.5px"
+        marginBottom: "-3.5px",
+        "&:hover svg": {
+            color: "white",
+            transform: "scale(1.5)"
+        }
+    }),
+    boxContent: css({
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        padding: "10px",
+        color: "rgba(0, 0, 0, 0.5)",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+        display: "flex",
+        justifyContent: "space-between"
+    }),
+    deleteIcon: css({
+        transition: "all 0.3s ease-in-out"
     })
-}
+};
 
 const DraggableColorBox = (props) => {
     return (
         <div css={styles.root} style={{ backgroundColor: props.color }}>
-            {props.name}
+            <div css={styles.boxContent}>
+                <span>{props.name}</span>
+                <DeleteIcon css={styles.deleteIcon} />
+            </div>
         </div>
     )
 }
