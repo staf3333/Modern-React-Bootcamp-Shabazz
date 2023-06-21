29e0ee0ad6b55ab6d83014e8a9bc7a9302815ed8
staf3333 <staf3333@gmail.com>
2023-06-20 19:31:47 -0700

    save palettes to local storage

--- Massive_Color_Project/react-colors-app/src/App.js
+++ Massive_Color_Project/react-colors-app/src/App.js
@@ -2,19 +2,28 @@ import './styles/App.css';
 import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PaletteWrapper from './PaletteWrapper';
 import NewPaletteForm from './NewPaletteForm';
 import SingleColorPaletteWrapper from './SingleColorPaletteWrapper';
 
 function App() {
+  const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
   const [state, setState] = useState({
-    palettes: seedColors
-  })
+    palettes: savedPalettes || seedColors
+  });
   const { palettes } = state;
-  const savePalette = (newPalette) => {
-    setState({ ...state, palettes: [...state.palettes, newPalette] })
+  const syncLocalStorage = () => {
+    //save palettes to local storage
+    window.localStorage.setItem("palettes", JSON.stringify(state.palettes));
   }
+  const savePalette = (newPalette) => {
+    setState({ ...state, palettes: [...state.palettes, newPalette] });
+  }
+  useEffect(() => {
+    syncLocalStorage();
+  }, [state])
+
 
   return (
     <Routes>
@@ -23,9 +32,6 @@ function App() {
       <Route path='/palette/:id' element={<PaletteWrapper palettes={palettes} />} />
       <Route path='/palette/:paletteId/:colorId' element={<SingleColorPaletteWrapper palettes={palettes} />} />
     </Routes>
-    // <div>
-    //   <Palette palette={generatePalette(seedColors[4])} />
-    // </div>
   );
 }
 
