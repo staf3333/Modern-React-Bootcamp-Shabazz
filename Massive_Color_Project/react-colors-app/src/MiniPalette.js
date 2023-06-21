f98b20cb88e2ce4e761bd9bd98061914bd5a3348
staf3333 <staf3333@gmail.com>
2023-06-20 19:40:44 -0700

    add deleteIcon to MiniPalette

--- Massive_Color_Project/react-colors-app/src/MiniPalette.js
+++ Massive_Color_Project/react-colors-app/src/MiniPalette.js
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import miniPaletteCss from './styles/MiniPaletteStyles'
+import miniPaletteCss from './styles/MiniPaletteStyles';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const MiniPalette = (props) => {
     const { paletteName, emoji, colors, handleClick } = props;
@@ -12,6 +13,9 @@ const MiniPalette = (props) => {
     ))
     return (
         <div css={miniPaletteCss.root} onClick={handleClick}>
+            <div css={miniPaletteCss.delete}>
+                <DeleteIcon css={miniPaletteCss.deleteIcon} style={{ transition: "all 0.3s ease-in-out" }} />
+            </div>
             <div css={miniPaletteCss.colors}>
                 {miniColorBoxes}
             </div>
--- Massive_Color_Project/react-colors-app/src/styles/MiniPaletteStyles.js
+++ Massive_Color_Project/react-colors-app/src/styles/MiniPaletteStyles.js
@@ -6,8 +6,9 @@ const styles = {
         borderRadius: "5px",
         padding: "0.5rem",
         position: "relative",
-        "&:hover": {
-            cursor: "pointer"
+        cursor: "pointer",
+        "&:hover svg": {
+            opacity: 1
         }
     }),
     colors: css({
@@ -38,6 +39,22 @@ const styles = {
         margin: "0 auto",
         position: "relative",
         marginBottom: "-3.5px"
+    }),
+    delete: css({
+
+    }),
+    deleteIcon: css({
+        color: "white",
+        backgroundColor: "#eb3d30",
+        height: "20px",
+        width: "20px",
+        position: 'absolute',
+        right: "0px",
+        top: "0px",
+        padding: "10px",
+        zIndex: 10,
+        opacity: 0,
+
     })
 }
 
