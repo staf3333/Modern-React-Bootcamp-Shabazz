4cb88a5a845ac71f4ed70ad3d4d75c1d9d5c0942
staf3333 <staf3333@gmail.com>
2023-06-25 19:10:27 -0700

    stop being stupid

--- 37_NextJS/first-next/app/page.js
+++ 37_NextJS/first-next/app/page.js
@@ -1,8 +1,26 @@
-export default function Page() {
-    console.log("running index component");
+import Link from 'next/link';
+
+const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+    return res.json();
+}
+
+const Page = async () => {
+    const posts = await getData();
     return (
         <div>
             <h1>Our Index Page!!!</h1>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link href="/post">
+                            {post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
+
+export default Page;
