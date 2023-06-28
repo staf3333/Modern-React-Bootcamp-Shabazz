29d06c3db616a0bf674c4a4c155618c4060681c9
staf3333 <staf3333@gmail.com>
2023-06-27 18:20:53 -0700

    add entry point for vendor.js, add bootstrap

--- 39_Webpack_MiniCourse/webpack-demo/webpack.prod.js
+++ 39_Webpack_MiniCourse/webpack-demo/webpack.prod.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: "main.[contenthash].js",
+        filename: "[name].[contenthash].bundle.js",
         clean: true,
         path: path.resolve(__dirname, "dist")
     }
